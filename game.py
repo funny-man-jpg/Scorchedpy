3863550a2664650201017a9024f6a0e71dd33e9e
Sabin Caragea <sabincaragea2005@gmail.com>
2021-02-07 11:53:07 -0500

    Load background and tank images

--- game.py
+++ game.py
@@ -9,6 +9,33 @@ class Game:
     def display_frame(self, screen):
         """ Display everything to the screen for the game. """
         screen.fill((255, 255, 255))
+
+        
+        width = 800
+        height = 600
+        background_image = pygame.image.load('assets/background.jpg')
+        scaled_background = pygame.transform.smoothscale(background_image, (width, height))
+        pos_x = 0
+        pos_y = 0
+        screen.blit(scaled_background, (pos_x, pos_y))
+
+
+        width = 50
+        height = 50
+        tank1_image = pygame.image.load('assets/tank1.png')
+        scaled_tank1 = pygame.transform.smoothscale(tank1_image, (width, height))
+        pos_x = 100
+        pos_y = 300
+        screen.blit(scaled_tank1, (pos_x, pos_y))
+
+        width = 50
+        height = 50
+        tank2_image = pygame.image.load('assets/tank2.png')
+        scaled_tank2 = pygame.transform.smoothscale(tank2_image, (width, height))
+        scaled_tank2 = pygame.transform.flip(tank2_image, True, False)
+        pos_x = 650
+        pos_y = 300
+        screen.blit(scaled_tank2, (pos_x, pos_y))
         
         pygame.display.flip()
 
