e74a2b96eb525c3147dbda3d9c024741a0173588
Sabin Caragea <sabincaragea2005@gmail.com>
2021-08-07 14:45:10 -0400

    Enemy AI assignment

--- agent.py
+++ agent.py
@@ -9,17 +9,37 @@ class Agent(Drawable):
         self.vel_y = vel_y
         self.accel_x = 0
         self.accel_y = 0
-
-    def collided(self, other):
-        if not isinstance(other, Agent):
-            return False
-
-        if (self.pos_x < other.pos_x + other.width and self.pos_x + self.width > other.pos_x):
-            if (self.pos_y < other.pos_y + other.height and self.pos_y + self.height > other.pos_y):
-                return True
+        self.collidable = True
+        self.collideCountdown = 0
 
     def update(self):
         self.vel_x += self.accel_x
         self.vel_y += self.accel_y
         self.pos_x += self.vel_x
         self.pos_y += self.vel_y
+        if self.collideCountdown != 0:
+            self.collideCountdown -= 1
+        if self.collideCountdown == 0:
+            self.collidable = True
+
+    def setNotCollidable(self, timeSteps=10):
+        self.collidable = False
+        self.collideCountdown = timeSteps
+
+    def collided(self, other):
+        if not isinstance(other, Agent):
+            return False
+
+        if not other.collidable:
+            return False
+
+        if (
+            self.pos_x + self.width > other.pos_x
+            and self.pos_x < other.pos_x + other.width
+        ):
+            if (
+                self.pos_y + self.height > other.pos_y
+                and self.pos_y < other.pos_y + other.height
+            ):
+                return True
+        return False
--- game.py
+++ game.py
@@ -19,6 +19,8 @@ from player import Player
 from missiles import Missile
 from hud import HUD
 from sprite import Sprite
+from enemy import Enemy
+import random
 
 
 
@@ -38,22 +40,23 @@ class Game:
         self.player.loadFile('assets/tank2_sprite.png', 1, 13)
         self.player.setAnimated(False)
         self.player.setIndex(0)
-        self.enemy = Player(600, self.ground_y - 30, 50, 50)
-        self.enemy.loadFile('assets/tank3_sprite.png', 1, 13)
-        self.enemy.flip(True, False)
-        self.enemy.setAnimated(False)
-        self.enemy.setIndex(12)
+        enemy = Enemy(600, self.ground_y - 30, 50, 50)
+        enemy.loadFile('assets/tank3_sprite.png', 1, 13)
+        enemy.flip(True, False)
+        enemy.setAnimated(False)
+        enemy.setIndex(0)
         
         self.players = []
         self.players.append(self.player)
-        self.players.append(self.enemy)
+        self.players.append(enemy)
         self.missiles = []
         self.craters = []
         self.explosions = []
         self.hud = HUD(0, 0, screen_w, 100)
         self.hud.setPower(self.player.power)
         self.hud.setAngle(self.player.angle)
-        
+        self.hasShot = False
+        self.currentPlayerIndex = 0
 
     def display_frame(self, screen):
         """ Display everything to the screen for the game. """
@@ -95,19 +98,24 @@ class Game:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return True
+            
             if event.type == KEYDOWN:
+                if type(self.player) != Player:
+                    return False
                 if event.key == K_SPACE:
-                    vel_x = self.player.calc_player_vel_x()
-                    vel_y = self.player.calc_player_vel_y()
-                    missile = Missile(
-                        self.player.pos_x+self.player.width /2 + vel_x*30,
-                        self.player.pos_y + 15 + vel_y*30, 
-                        4, 
-                        4, 
-                        self.player.calc_player_vel_x(),
-                        self.player.calc_player_vel_y()
-                        )
-                    self.missiles.append(missile) 
+                    if self.hasShot == False:
+                        vel_x = self.player.calc_player_vel_x()
+                        vel_y = self.player.calc_player_vel_y()
+                        missile = Missile(
+                            self.player.pos_x+self.player.width /2 + vel_x*30,
+                            self.player.pos_y + 15 + vel_y*30, 
+                            4, 
+                            4, 
+                            self.player.calc_player_vel_x(),
+                            self.player.calc_player_vel_y()
+                            )
+                        self.missiles.append(missile)
+                        self.hasShot = True 
                 if event.key == K_UP:
                     self.player.power += powerDelta
                 if event.key == K_DOWN:
@@ -135,17 +143,30 @@ class Game:
 
     def onMissileHit(self, missile, pos_x, pos_y):
         print("Collided")
-        self.missiles.remove(missile)
+        try:
+            self.missiles.remove(missile)
+            
+            crater = Drawable(pos_x, pos_y + 30, 64, 20)
+            crater.loadFile('assets/crater.png')
+            self.craters.append(crater)
+
+
+            explosion = Sprite(pos_x - 25, pos_y - 50, 100, 100)
+            explosion.loadFile('assets/boom.png', 3, 4)
+            explosion.start()
+            self.explosions.append(explosion)
+            self.next_player()
+        except:
+            print(f"Error onMissileHit")
+
+    def next_player(self):
+        self.currentPlayerIndex += 1
+        self.currentPlayerIndex = self.currentPlayerIndex % len(self.players)
+
+        self.player = self.players[self.currentPlayerIndex]
+        self.hasShot = False
+        self.player.collidable = True
         
-        crater = Drawable(pos_x, pos_y + 30, 64, 20)
-        crater.loadFile('assets/crater.png')
-        self.craters.append(crater)
-
-
-        explosion = Sprite(pos_x - 25, pos_y - 50, 100, 100)
-        explosion.loadFile('assets/boom.png', 3, 4)
-        explosion.start()
-        self.explosions.append(explosion)
 
     def run(self):
         done = False
@@ -164,6 +185,24 @@ class Game:
             for player in self.players:
                 if isinstance(player, Agent):
                     player.update()
+            
+            if type(self.player) == Enemy:
+                if self.hasShot == False:
+                    self.player.power = random.randrange(10, 100)
+                    self.player.angle = random.randrange(180, 360)
+                    vel_x = self.player.calc_player_vel_x()
+                    vel_y = self.player.calc_player_vel_y()
+                    missile = Missile(
+                        self.player.pos_x-self.player.width /2 - vel_x*30,
+                        self.player.pos_y + 15 + vel_y*30, 
+                        4, 
+                        4, 
+                        self.player.calc_player_vel_x(),
+                        self.player.calc_player_vel_y()
+                        )
+                    self.missiles.append(missile)
+                    self.player.setNotCollidable()
+                    self.hasShot = True 
 
             for missile in self.missiles:
                 if isinstance(missile, Agent):
